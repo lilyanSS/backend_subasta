a6b3259738164244a601455bf00ea7f29701b674
Lilyan <lilyan.sica@hummingbirdit.tech>
2020-10-18 22:19:08 -0600

    agregar rutas y agregar modelos de catalogo

--- subasta_backend/urls.py
+++ subasta_backend/urls.py
@@ -1,4 +1,4 @@
-"""subasta URL Configuration
+"""subasta_backend URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+# from apps.user import user
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('users/', include('apps.user.urls')),
+    path('subastas/', include('apps.subastas.urls')),
+    path('catalogo/', include('apps.catalogo.urls')),
 ]
