136cbb4e721efd8289114e6fbfa7dbb5575b59ff
Lilyan <lilyan.sica@hummingbirdit.tech>
2020-10-29 20:51:53 -0600

    quitar archivos innecesarios

---
+++ apps/auction/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth import authenticate, get_user_model
+User = get_user_model()
+# Create your models here.
+class Vehicle_in_auction(models.Model):
+    base_price = models.FloatField(verbose_name="base_price")
+    creation_date = models.DateField(default=timezone.now)
+    on_sale = models.BooleanField(default=True)
+    admin = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
+    id_vehicle= models.IntegerField()
+
+
+
+
--- apps/auction/urls.py
+++ apps/auction/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 
 from . import views
 
-app_name = 'subastas'
+app_name = 'auction'
 urlpatterns = [
 
 ]
