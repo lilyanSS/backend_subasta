25d698fe0d55e589be8e80e789c06ccf1931ac44
Lilyan <lilyan.sica@hummingbirdit.tech>
2020-11-04 22:24:52 -0600

    agregar configuracion para base de datos

--- subasta_backend/settings.py
+++ subasta_backend/settings.py
@@ -116,14 +116,14 @@ STATICFILES_DIRS = (
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        # 'ENGINE': 'django.db.backends.mysql', 
-        # 'NAME': 'subasta',
-        # 'USER': 'lilyan',
-        # 'PASSWORD': 'admin',
-        # 'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
-        # 'PORT': '3306',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'subasta',
+        'USER': 'jaeger',
+        'PASSWORD': 'jaeger-admin',
+        'HOST': '157.230.217.60',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
 
