4a8d0b566d6385bc4b6bc786e6b04ee65c1bdcab
Lilyan <lilyan.sica@hummingbirdit.tech>
2020-11-04 23:48:30 -0600

    cambio nombre

--- subasta_backend/settings.py
+++ subasta_backend/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 import dj_database_url
 from decouple import config
-import django_heroku
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'apps.users',
     'corsheaders',
 ]
-django_heroku.settings(locals())
+
 # DJANGO REST FRAMEWORK
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
