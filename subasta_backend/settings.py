2890c7ff4c7168300b97728fffaea5935671152f
Lilyan <lilyan.sica@hummingbirdit.tech>
2020-11-12 18:36:22 -0600

    limite de pagina

--- subasta_backend/settings.py
+++ subasta_backend/settings.py
@@ -58,7 +58,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 15,
+    # 'PAGE_SIZE': 15,
     
 }
 
